47b9a63d3eb0bb400f499709896c6b3a57d5c1df
Pawel Karpinski <pawelka@gmail.com>
2018-11-16 12:50:16 +0100

    update zigate version

--- zigate/__init__.py
+++ zigate/__init__.py
@@ -22,7 +22,7 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['zigate==0.21.1']
+REQUIREMENTS = ['zigate==0.21.2']
 DEPENDENCIES = ['persistent_notification']
 
 DOMAIN = 'zigate'
