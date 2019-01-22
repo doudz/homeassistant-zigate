0f32c2405cde2abf006f2c880d470d83b37da851
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-01-22 15:54:12 +0100

    Update lib to 0.26.1 - fix lumi.ctrl_neutral1

--- zigate/__init__.py
+++ zigate/__init__.py
@@ -22,7 +22,7 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['zigate==0.26.0']
+REQUIREMENTS = ['zigate==0.26.1']
 DEPENDENCIES = ['persistent_notification']
 
 DOMAIN = 'zigate'
