edeaa378bb41913c76efe5f3d9c133b0b3ddc615
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-12-17 16:22:39 +0100

    handle get params

--- custom_components/zigate/__init__.py
+++ custom_components/zigate/__init__.py
@@ -764,7 +764,7 @@ class ZiGateProxy(HomeAssistantView):
 
     async def get(self, request, routename):
         """Handle ZiGate proxy requests."""
-        r = requests.get('http://localhost:9998/'+routename)
+        r = requests.get('http://localhost:9998/'+routename, params=request.query)
         headers = r.headers.copy()
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT'
