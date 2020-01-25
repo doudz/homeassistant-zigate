ecf456584b450c598001653ac8b3a56891c7575b
cyr-ius <cyr-ius@ipocus.net>
2020-01-25 12:03:20 +0100

    Restructuration du code

--- custom_components/zigate/const.py
+++ custom_components/zigate/const.py
@@ -1,3 +1,14 @@
-
 DOMAIN = 'zigate'
 SCAN_INTERVAL = 120
+DATA_ZIGATE_DEVICES = 'zigate_devices'
+DATA_ZIGATE_ATTRS = 'zigate_attributes'
+ADDR = 'addr'
+IEEE = 'ieee'
+SUPPORTED_PLATFORMS = (
+    'sensor',
+    'binary_sensor',
+    'switch',
+    'light',
+    'cover',
+    'climate'
+)
---
+++ custom_components/zigate/core/admin_panel.py
@@ -0,0 +1,69 @@
+from homeassistant.components.http import HomeAssistantView
+
+BASE_PANEL = '''
+<dom-module id='ha-panel-zigateadmin'>
+  <template>
+    <iframe src="/zigateproxy/" style="width:99%; height:99%; border:0"></iframe>
+  </template>
+</dom-module>
+
+<script>
+class HaPanelZiGateadmin extends Polymer.Element {
+  static get is() { return 'ha-panel-zigateadmin'; }
+
+  static get properties() {
+    return {
+      // Home Assistant object
+      hass: Object,
+      // If should render in narrow mode
+      narrow: {
+        type: Boolean,
+        value: false,
+      },
+      // If sidebar is currently shown
+      showMenu: {
+        type: Boolean,
+        value: false,
+      },
+      // Home Assistant panel info
+      // panel.config contains config passed to register_panel serverside
+      panel: Object
+    };
+  }
+}
+customElements.define(HaPanelZiGateadmin.is, HaPanelZiGateadmin);
+</script>
+'''
+
+
+class ZiGateAdminPanel(HomeAssistantView):
+    requires_auth = False
+    name = "zigateadmin"
+    url = "/zigateadmin.html"
+
+    async def get(self, request):
+        """Handle ZiGate admin panel requests."""
+        return web.Response(text=BASE_PANEL)
+
+
+class ZiGateProxy(HomeAssistantView):
+    requires_auth = False
+    cors_allowed = True
+    name = "zigateproxy"
+    url = "/zigateproxy/{routename:.*}"
+
+    async def get(self, request, routename):
+        """Handle ZiGate proxy requests."""
+        headers = {
+            "Cache-Control": "no-cache",
+            "Pragma": "no-cache"
+        }
+        r = requests.get('http://localhost:9998/'+routename, params=request.query, headers=headers)
+        headers = r.headers.copy()
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT'
+        headers['Cache-Control'] = 'no-cache'
+        headers['Pragma'] = 'no-cache'
+        return web.Response(body=r.content, status=r.status_code, headers=headers)
+
+
